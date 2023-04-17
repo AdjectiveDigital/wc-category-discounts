fe7f0637423b62cb1a2db1d164d8a0c24adc8672
gs <gs@GS.local>
2023-04-17 12:48:31 +1000

    bug fixes

--- includes/class-wc-category-discounts.php
+++ includes/class-wc-category-discounts.php
@@ -30,11 +30,21 @@ class Wc_Category_Discounts
         // Apply the discount to product prices.
         add_filter('woocommerce_product_get_price', array($this, 'apply_discount'), 10, 2);
         add_filter('woocommerce_product_variation_get_price', array($this, 'apply_discount'), 10, 2);
+        add_filter('woocommerce_get_price_html', array($this, 'wc_category_discounts_sale_price_html'), 10, 2);
+        add_filter('woocommerce_variable_sale_price_html', array($this, 'wc_category_discounts_variable_price_html'), 10, 2);
+        add_filter('woocommerce_variable_price_html', array($this, 'wc_category_discounts_variable_price_html'), 10, 2);
+
     }
 
     public static function apply_discount($price, $product)
     {
         $product_id = $product->get_id();
+        $regular_price = (float) $product->get_regular_price();
+        $sale_price = $product->get_sale_price();
+
+        if (!empty($sale_price)) {
+            $sale_price = (float) $sale_price;
+        }
 
         if ($product->is_type('variation')) {
             $parent_id = $product->get_parent_id();
@@ -49,12 +59,13 @@ class Wc_Category_Discounts
             $discount_data = self::get_discount_for_category($category);
             $discount_type = $discount_data['type'];
             $discount_value = $discount_data['value'];
+            $apply_to_sale_items = get_option("wc_category_discounts_apply_to_discounted_items_{$category->term_id}") === '1';
 
             if ($discount_type && $discount_value) {
                 $discount = 0;
 
                 if ($discount_type === 'percentage') {
-                    $discount = $price * ($discount_value / 100);
+                    $discount = $regular_price * ($discount_value / 100);
                 } elseif ($discount_type === 'fixed') {
                     $discount = $discount_value;
                 }
@@ -63,28 +74,37 @@ class Wc_Category_Discounts
                     $max_discount = $discount;
                 }
             }
+
         }
 
         if ($max_discount > 0) {
-            $price = $price - $max_discount;
+            $discounted_price = $regular_price - $max_discount;
+
+            if ($sale_price === '' || $apply_to_sale_items && $discounted_price < $sale_price) {
+                $price = $discounted_price;
+            } elseif ($sale_price !== '' && !$apply_to_sale_items) {
+                $price = $sale_price;
+            }
         }
 
         return $price;
     }
 
+
+
     public static function wc_category_discounts_sale_price_html($price_html, $product)
     {
-        if ($product->is_type('simple')) {
-            $regular_price = $product->get_regular_price();
-            $discounted_price = self::apply_discount($regular_price, $product);
+        $regular_price = $product->get_regular_price();
+        $sale_price = $product->get_sale_price();
+        $discounted_price = self::apply_discount($regular_price, $product);
 
-            if ($discounted_price != $regular_price) {
-                $price_html = '<del>' . wc_price($regular_price) . '</del> <ins>' . wc_price($discounted_price) . '</ins>';
-            }
+        if ($discounted_price != $regular_price && ($sale_price === '' || $discounted_price < $sale_price)) {
+            $price_html = '<del>' . wc_price($regular_price) . '</del> <ins>' . wc_price($discounted_price) . '</ins>';
         }
 
         return $price_html;
     }
+
 
     public static function get_discount_for_category($category)
     {
@@ -103,6 +123,36 @@ class Wc_Category_Discounts
 
         return $discount_data;
     }
-}
 
-add_filter('woocommerce_get_price_html', array('Wc_Category_Discounts', 'wc_category_discounts_sale_price_html'), 10, 2);
+    public static function wc_category_discounts_variable_price_html($price_html, $product)
+    {
+        $variations = $product->get_available_variations();
+        $min_discounted_price = null;
+        $max_discounted_price = null;
+
+        foreach ($variations as $variation_data) {
+            $variation = wc_get_product($variation_data['variation_id']);
+            $regular_price = $variation->get_regular_price();
+            $discounted_price = self::apply_discount($regular_price, $variation);
+
+            if ($min_discounted_price === null || $discounted_price < $min_discounted_price) {
+                $min_discounted_price = $discounted_price;
+            }
+
+            if ($max_discounted_price === null || $discounted_price > $max_discounted_price) {
+                $max_discounted_price = $discounted_price;
+            }
+        }
+
+        if ($min_discounted_price !== null && $max_discounted_price !== null) {
+            if ($min_discounted_price !== $max_discounted_price) {
+                $price_html = wc_format_price_range($min_discounted_price, $max_discounted_price);
+            } else {
+                $price_html = wc_price($min_discounted_price);
+            }
+        }
+
+        return $price_html;
+    }
+
+}
